98ce07170618cd739ed108538652cead3b463480
Caylia <113325942+cayliab21@users.noreply.github.com>
2023-11-20 19:16:43 -0500

    Update GameMechs.cpp

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,53 +1,120 @@
 #include "GameMechs.h"
+#include "MacUILib.h"
 
 GameMechs::GameMechs()
 {
+    input = NULL;
+    exitFlag = false;
+    loseFlag = false;
+
+    score = 0; // initializing score
+    boardSizeX = 20; // default board size
+    boardSizeY = 10;
+
+    SLOWEST_SPEED = 200000; // Delay in microseconds for speed level 1 (slowest)
+    SLOW_SPEED = 150000;
+    INITIAL_SPEED = 100000; // Represents 100 milliseconds (or 0.1 seconds) delay between game loops
+    FAST_SPEED = 50000;
+    FASTEST_SPEED = 20000;// Delay in microseconds for speed level 5 (fastest)
+
+    speedLevel = 3; // Initial speed level is set to 3
 
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
 
+    input = NULL;
+    exitFlag = false;
+    loseFlag = false;
+
+    score = 0;
+    boardSizeX = boardX;  
+    boardSizeY = boardY;
+
+    SLOWEST_SPEED = 200000; // Delay in microseconds for speed level 1 (slowest)
+    SLOW_SPEED = 150000;
+    INITIAL_SPEED = 100000; // Represents 100 milliseconds (or 0.1 seconds) delay between game loops
+    FAST_SPEED = 50000;
+    FASTEST_SPEED = 20000; // Delay in microseconds for speed level 5 (fastest)
+
+    speedLevel = 3; // Initial speed level is set to 3
+
 }
 
 // do you need a destructor?
 
-
+/*
+GameMechs::~GameMechs() {
+    // for adding cleanup code
+}
+*/
 
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
+
+bool GameMechs::getLoseFlagStatus()
+{
+    return loseFlag;
+}
+
 
 char GameMechs::getInput()
 {
+    if (MacUILib_hasChar()) {
+        tempInput = MacUILib_getChar();
 
+        if (tempInput == 27) {
+            exitFlag = 1; // Terminate program on escape key
+        } else if (tempInput == '+') { // When + is pressed, speed will increase up the the max of 5
+            if (speedLevel < 5) {
+                speedLevel++;
+            }
+        } else if (tempInput == '-') { // When - is pressed, speed will decrease down the the min of 1
+            if (speedLevel > 1) {
+                speedLevel--;
+            }
+        } else {
+            input = tempInput; // Update regular input
+        }
+    }
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
+int GameMechs::getScore()
+{
+    return score;
+}
 
 void GameMechs::setExitTrue()
 {
+    exitFlag = true;
+}
 
+void GameMechs::setLoseFlag()
+{
+    loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+ input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
 
 
